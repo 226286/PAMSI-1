22c7c1a69f9634048460c2f6e832f4c0167c6626
218556 <218556@student.pwr.edu.pl>
2017-03-29 18:52:23 +0200

    Program od Bruce'a Eckela. Działa. Teraz dopasować do swoich potrzeb, dorobić pomiar czasu i git.

--- Lab2/inc/dynamicarray.hh
+++ Lab2/inc/dynamicarray.hh
@@ -1,18 +1,45 @@
 #ifndef DynamicArray_HH
 #define DynamicArray_HH
 
-class DynamicArray
+
+    //: C04:CppLib.h
+    // C-like library converted to C++
+
+    struct Stash {
+      int size;      // Size of each space
+      int quantity;  // Number of storage spaces
+      int next;      // Next empty space
+       // Dynamically allocated array of bytes:
+      unsigned char* storage;
+      // Functions!
+      void initialize(int size);
+      void cleanup();
+      int add(const void* element);
+      void* fetch(int index);
+      int count();
+      void inflate(int increase);
+    }; ///:~
+
+
+
+/*class DynamicArray
 {
 	private:
-		void PowiekszTablice();
-		void PomniejszTablice();
-		void AktualnyStan();
+		int size;      // Size of each space
+		int quantity;  // Number of storage spaces
+		int next;      // Next empty space
+		unsigned char* storage;// Dynamically allocated array of bytes:
+		
+		
+		void EnlargeAnArray();
+		void ReturnAnElement();
+		void NumberOfElements();
 	public:
 		DynamicArray();
 		~DynamicArray();
 		void AddNewElement();
-		void Display();
+		void Print();
 	
-};
+};*/
 
 #endif
