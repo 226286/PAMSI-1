2b5052013721f9addbe1bb534fb71c5d62776d7f
218556 <218556@student.pwr.edu.pl>
2017-04-06 18:19:21 +0200

    Trochę zepsułam. Trzeba przerobić funkcję quicksort na DynamicArray zamiast int.

--- Lab5/prj/src/main.cpp
+++ Lab5/prj/src/main.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <ctime>
+#include <cstdlib>
 #include "../inc/dynamicarray.hh"
 #include "../inc/quicksort.hh"
 using namespace std;
@@ -10,13 +11,31 @@ using namespace std;
 
 int main()
 {
+	srand( time( NULL ) );
+	int nr_of_elem=5;
+	DynamicArray tablica(sizeof(int));
+	for(int i=0; i<nr_of_elem; i++)
+	{
+		int x = (rand() % 10 ) + 1;
+		tablica.AddNewElement(&x);
+	}
+	tablica.Print();
+	int n = tablica.NumberOfElements();
+	quicksort((int*)tablica, 0, n-1);
+	tablica.Print();
+	
+	
+	
+	
+	
+	/*
 	int tablica[] = {5, 4, 2, 1, 3};
 	int n = sizeof(tablica)/sizeof(int);
-	quicksort(tablica, 0, n-1);
+
 	for (int i=0; i<n; i++)
 	{
 		cout<<tablica[i]<<" ";
 	}
-	cout<<endl;
+	cout<<endl;*/
 	return 0;
 }
