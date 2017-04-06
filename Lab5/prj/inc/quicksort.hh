4ae7fa67cd7ad4c1e47aec23db905b61b4a03c49
218556 <218556@student.pwr.edu.pl>
2017-04-06 16:02:18 +0200

    Quicksort - naruszenie ochrony pamięci, ale jak to naprawię, to powinno działać

--- Lab5/prj/inc/quicksort.hh
+++ Lab5/prj/inc/quicksort.hh
@@ -1,28 +1,25 @@
 #ifndef Quicksort_HH
 #define Quicksort_HH
 
-int partition(int& array, int p, int r)
+int partition(int array[], int p, int r)
 {
 	int x = array[p];
 	int i=0, j=0;
-	for (i = p; array[i] >= x; i++){}
-	for (j = r; array[j] <= x; j--){}
+	for (i = p-1; array[i] >= x; i++){}
+	for (j = r+1; array[j] <= x; j--){}
 	if(i < j)
 		{
 			int tmp = array[i];
 			array[i] = array[j];
 			array[j] = tmp;
 		}
-	else
-		{
-			return j;
-		}
+	return j;
 }
 
 
 
 
-void quicksort(int& array, int p, int r)
+void quicksort(int array[], int p, int r)
 {
 	if(p < r)
 	{
